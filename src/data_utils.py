1dc40080e411ad4f3e2603fdda7e7aec6293a2be
Dominik Lau <harek2388@gmail.com>
2023-03-16 20:24:00 +0100

    begin implementing data recording

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,4 +1,9 @@
+import pygame
+import sounddevice as sd
+
+
 from pandas import read_csv
+from scipy.io.wavfile import write
 
 
 def label_to_sample(filename, timestamps):
@@ -12,3 +17,27 @@ def label_to_sample(filename, timestamps):
         labels.append(all_data[current][0])
 
     return labels
+
+
+def data_recorder(filename):
+
+    pygame.init()
+    screen = pygame.display.set_mode((740, 480))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.KEYUP:
+                pass
+
+        screen.fill((0, 0, 0))
+        pygame.display.update()
+    # sample_rate = 44100
+    # chunk = 1024
+    # channels = 1
+    # seconds = 3
+    # rec = sd.rec(int(seconds * sample_rate), samplerate=sample_rate, channels=channels)
+    # sd.wait()
+    # write('output.wav', sample_rate, rec)
