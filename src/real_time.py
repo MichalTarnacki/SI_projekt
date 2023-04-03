2b9201d79f8e86ac49e623336539a3c0cda480c3
Dominik Lau <harek2388@gmail.com>
2023-04-03 18:25:39 +0200

    reduced amount of frequencies taken into account, removed noisereduce, added real time visualization, added savgol filter

--- src/real_time.py
+++ src/real_time.py
@@ -10,6 +10,8 @@ import pygame
 import pyaudio
 import sounddevice as sd
 import soundcard as sc
+from scipy.signal import savgol_filter
+
 import src.data_engineering.spectrogram as sp
 
 from pydub import effects
@@ -57,22 +59,27 @@ def detection(model, scaler, chunk_size=352, input_size=40, uses_previous_state=
                 write('test.wav', fs, samples)
                 pygame.quit()
                 return
+        screen.fill((0, 0, 0))
+
         if samples.shape[0] > chunk_size * (input_size + 200):
-            clean = noisereduce.reduce_noise(samples, 44100)
-            # clean = samples
+            # clean = noisereduce.reduce_noise(samples[-1024:], 44100)
+
+            clean = samples
             last_frame = abs(np.fft.rfft(clean[len(clean) - sp.CHUNK_SIZE:]))[:160]
+            last_frame = sp.signal_clean(last_frame)
             if uses_previous_state:
                 last_frame = np.append(last_frame, 1 if prev_state == 'in' else -1)
                 prev_state = state
-
-            plt.clf()
-            plt.plot(last_frame)
-            plt.pause(0.001)
+            # clean_frame = sp.signal_clean(last_frame[:-1])
+            for x, y in enumerate(last_frame[:-1]):
+                pygame.draw.line(screen, (255, 0, 0) if state == 'in' else (0, 255, 0), (x*3, 480 - y), (x*3 + 3, 480 - last_frame[x+1]))
 
             last_frame_std = scaler.transform(last_frame.reshape(-1, 1).T)
             state = model.predict(last_frame_std)
 
-        screen.fill((0, 0, 0))
+            # if samples.shape[0] > 44100:
+                # samples = samples[1024:]
+
         text = font.render('teraz wdychasz' if state == 'in' else 'teraz wydychasz', True, (255, 255, 255))
         screen.blit(text, (0, 0))
         pygame.display.update()
